291d8857d7cf6e996eedd1e92e6cfa7bbe8c3cdf
Miles Tallia (RIT Student) <mot7506@rit.edu>
2025-02-22 16:55:11 -0500

    maximized windows and paper remade to draggable

--- middle-mismanagement/Scripts/Game.gd
+++ middle-mismanagement/Scripts/Game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var paper_stack = []
+var obj_stack = []
 
 var paper_scene = preload("res://Scenes/Paper.tscn")
 
@@ -8,17 +8,17 @@ var paper_scene = preload("res://Scenes/Paper.tscn")
 func _ready():
 	for i in range(0, 2):
 		var paper = paper_scene.instantiate()
-		add_paper(paper)
+		add_obj(paper)
 
-func add_paper(paper):
-	paper_stack.append(paper)
+func add_obj(obj):
+	obj_stack.append(obj)
 	
 	var count = 0
-	for p in paper_stack:
+	for p in obj_stack:
 		p.z_index = count
 		
 		count += 1
 
-func push_paper_to_top(paper):
-	paper_stack.erase(paper)
-	add_paper(paper)
+func push_obj_to_top(obj):
+	obj_stack.erase(obj)
+	add_obj(obj)
