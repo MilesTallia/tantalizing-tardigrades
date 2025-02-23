00df34867647969225ab897c8ded33a47d92bb20
Miles Tallia (RIT Student) <mot7506@rit.edu>
2025-02-23 04:56:08 -0500

    upgraded credits

--- middle-mismanagement/Scripts/Credit.gd
+++ middle-mismanagement/Scripts/Credit.gd
@@ -12,3 +12,20 @@ func _process(delta: float) -> void:
 	
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Launch.tscn")
+
+
+
+func _on_miles_pressed() -> void:
+	OS.shell_open("https://www.linkedin.com/in/miles-tallia/")
+
+
+func _on_gabriel_pressed() -> void:
+	OS.shell_open("https://www.linkedin.com/in/gabriel-williams-17a400272/")
+
+
+func _on_andrew_pressed() -> void:
+	OS.shell_open("https://www.linkedin.com/in/andrew-dimauro-765565311/")
+
+
+func _on_max_pressed() -> void:
+	OS.shell_open("https://linkedin.com/in/maxrimlinger")
